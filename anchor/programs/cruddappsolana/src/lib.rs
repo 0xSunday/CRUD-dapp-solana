739d46845f9ba964cfefea9a1e32c5294708ef45
Your Name <sunday7637@gmail.com>
2025-02-10 16:44:06 +0000

    added delete

--- anchor/programs/cruddappsolana/src/lib.rs
+++ anchor/programs/cruddappsolana/src/lib.rs
@@ -22,9 +22,17 @@ pub mod cruddappsolana {
         Ok(())
     }
 
-    pub fn update_journal_entry(ctx: Context<UpdateEntry>,title:String, message: String) -> Result<()> {
+    pub fn update_journal_entry(
+        ctx: Context<UpdateEntry>,
+        title: String,
+        message: String,
+    ) -> Result<()> {
         let journal_entry = &mut ctx.accounts.journal_entry;
         journal_entry.message = message;
+        Ok(())
+    }
+
+    pub fn delete_journal_entry(ctx: Context<DeleteEntry>, title: String) -> Result<()> {
         Ok(())
     }
 }
@@ -63,8 +71,27 @@ pub struct UpdateEntry<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
 
-    pub system_program: Program<'info, System>, 
+    pub system_program: Program<'info, System>,
 }
+
+#[derive(Accounts)]
+#[instruction(title:String)]
+pub struct DeleteEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        close = owner,
+
+    )]
+    pub journal_entry: Account<'info, journalEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
 
 #[account]
 #[derive(InitSpace)]
