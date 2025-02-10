a49db7be063dbc42fb6ff37b6ad8588d1a0f43a7
Your Name <sunday7637@gmail.com>
2025-02-10 15:52:45 +0000

    added 'create'

--- anchor/programs/cruddappsolana/src/lib.rs
+++ anchor/programs/cruddappsolana/src/lib.rs
@@ -6,5 +6,47 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
 pub mod cruddappsolana {
+    use anchor_lang::context;
+
     use super::*;
+
+    pub fn create_journal_entry(
+        ctx: Context<CreateEntry>,
+        title: String,
+        message: String,
+    ) -> Result<()> {
+        let jouranl_entry = &mut ctx.accounts.journal_entry;
+        jouranl_entry.owner = *ctx.accounts.owner.key;
+        jouranl_entry.title = title;
+        jouranl_entry.message = message;
+        Ok(());
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(title:String)]
+pub struct CreateEntry<'info> {
+    #[account(
+    init,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    space = 8+ journalEntryState::INIT_SPACE,
+    payer = owner,
+ )]
+    pub journal_entry: Account<'info, journalEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct journalEntryState {
+    pub owner: Pubkey,
+    #[max_len(50)]
+    pub title: String,
+    #[max_len(1000)]
+    pub message: String,
 }
